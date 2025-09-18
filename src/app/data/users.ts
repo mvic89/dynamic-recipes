30c10a116ede76b42512cd1025e020fe1ecdbce8
Victor Furustubbe <mantykanto2@gmail.com>
2025-09-18 22:19:32 +0200

    added back some users just in case

--- src/app/data/users.ts
+++ src/app/data/users.ts
@@ -6,5 +6,23 @@ export const UserArray:UserType [] = [
     favouriteCategory: null,
     favouriteRecipe: [],
     password: "Olive Oil"
+  },
+  {
+    name: "Bobbe",
+    favouriteCategory: null,
+    favouriteRecipe: [],
+    password: "123"
+  },
+  {
+    name: "Robbe",
+    favouriteCategory: "Beef",
+    favouriteRecipe: [],
+    password: "456"
+  },
+  {
+    name: "Johnny",
+    favouriteCategory: "Fish",
+    favouriteRecipe: [],
+    password: "789"
   }
 ]
