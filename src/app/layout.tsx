cc058c4b6f93102c83f97fa687907416a8354739
Victor Furustubbe <mantykanto2@gmail.com>
2025-09-18 15:21:09 +0200

    added footer set below

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserContextProvider } from "./utils/contexts";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} h-full flex flex-col`}
       >
         <UserContextProvider>
           <Header/>
           {children}
+          <Footer/>
         </UserContextProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { user } = useUserContext() as UserContextType;
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-grow flex-col items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20">
       {user ? <p>Welcome to my site!</p> : <LogInForm />}
     </div>
   );
