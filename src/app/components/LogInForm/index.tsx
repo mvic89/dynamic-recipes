502a45476ce4dff968db4a7643caf81e05f53177
Victor Furustubbe <mantykanto2@gmail.com>
2025-09-18 22:05:06 +0200

    added changes to user data

--- src/app/components/LogInForm/index.tsx
+++ src/app/components/LogInForm/index.tsx
@@ -40,10 +40,10 @@ const handleClick = (event: React.FormEvent) => {
       <label
         htmlFor="username"
         className="text-sm font-medium text-gray-700 mb-1"
-      >Enter your username</label>
+      >Enter your username:</label>
       <input
         id="username"
-        placeholder="Username"
+        placeholder="Jamie Oliver"
         value={userInput}
         onChange={handleUsernameChange}
         className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500  focus:border-transparent"
@@ -53,10 +53,10 @@ const handleClick = (event: React.FormEvent) => {
       <label
         htmlFor="password"
         className="text-sm font-medium text-gray-700 mb-1"
-      >Enter your password</label>
+      >Enter your password:</label>
       <input
         id="password"
-        placeholder="Password"
+        placeholder="Olive Oil"
         type="password"
         value={passWordInput}
         onChange={handlePasswordChange}
--- src/app/data/users.ts
+++ src/app/data/users.ts
@@ -2,27 +2,9 @@ import { UserType } from "@/app/utils/types"
 
 export const UserArray:UserType [] = [
   {
-    name: "Tobbe",
+    name: "Jamie Oliver",
     favouriteCategory: null,
     favouriteRecipe: [],
-    password: "123"
-  },
-  {
-    name: "Bobbe",
-    favouriteCategory: null,
-    favouriteRecipe: [],
-    password: "456"
-  },
-  {
-    name: "Robbe",
-    favouriteCategory: "Beef",
-    favouriteRecipe: [],
-    password: "789"
-  },
-  {
-    name: "Johnny",
-    favouriteCategory: "Fish",
-    favouriteRecipe: [],
-    password: "123"
+    password: "Olive Oil"
   }
 ]
